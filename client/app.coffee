8e4930dc15d9b563fea5bd68ca368e1820d7f141
Tamino Martinius <dev@zaku.eu>
2013-05-30 23:05:09 +0200

    Fixed Analytics
    fixed Google-Analytics-loading
    moved from social.js to app.coffee

--- client/app.coffee
+++ client/app.coffee
@@ -413,3 +413,17 @@ Meteor.startup () ->
       return
 
   init()
+
+Template.analytics.rendered = ->
+  if !window._gaq?
+    window._gaq = []
+    _gaq.push(['_setAccount', 'UA-29865051-1'])
+    _gaq.push(['_setDomainName', 'colourco.de'])
+    _gaq.push(['_trackPageview'])
+
+    (->
+      ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      gajs = '.google-analytics.com/ga.js'
+      ga.src = if 'https:' is document.location.protocol then 'https://ssl'+gajs else 'http://www'+gajs
+      s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s)
+    )()
--- client/social.js
+++ client/social.js
@@ -11,14 +11,4 @@ Meteor.startup(function(){
     js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=277582698988224";
     fjs.parentNode.insertBefore(js, fjs);
   }(document, 'script', 'facebook-jssdk'));
-  //Analytics
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-29865051-1']);
-  _gaq.push(['_setDomainName', 'colourco.de']);
-  _gaq.push(['_trackPageview']);
-  (function () {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
 });
