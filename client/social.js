ba3a003a786c5c9995e3154c965163058aa9dafe
Tamino Martinius <dev@zaku.eu>
2013-06-12 16:45:52 +0200

    added small add below social media

--- client/social.js
+++ client/social.js
@@ -19,4 +19,6 @@ Meteor.startup(function(){
         s.src = 'http://api.flattr.com/js/0.6/load.js?mode=auto';
         t.parentNode.insertBefore(s, t);
     })();
+    //influads
+    (function(){var acc='acc_1318d914_pub';var st='nocss';var or='130';var e=document.getElementsByTagName('script')[0];var d=document.createElement('script');d.src=('https:' == document.location.protocol ?'https://' : 'http://') +'engine.influads.com/show/'+or+'/'+st+'/'+acc;d.type='text/javascript';d.async=true;d.defer=true; e.parentNode.insertBefore(d,e);})();
 });
