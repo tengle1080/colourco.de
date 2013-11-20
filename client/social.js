9006850b30f2c666318b3d917916de0e226b7e52
Tamino Martinius <dev@zaku.eu>
2013-11-20 19:23:50 +0100

    removed flattr popover - added btc donate

--- client/social.js
+++ client/social.js
@@ -16,9 +16,34 @@ Meteor.startup(function(){
         var s = document.createElement('script'), t = document.getElementsByTagName('script')[0];
         s.type = 'text/javascript';
         s.async = true;
-        s.src = 'http://api.flattr.com/js/0.6/load.js?mode=auto';
+        s.src = 'http://api.flattr.com/js/0.6/load.js?mode=auto&popout=0';
         t.parentNode.insertBefore(s, t);
     })();
     //influads
     (function(){var acc='acc_1318d914_pub';var st='nocss';var or='130';var e=document.getElementsByTagName('script')[0];var d=document.createElement('script');d.src=('https:' == document.location.protocol ?'https://' : 'http://') +'engine.influads.com/show/'+or+'/'+st+'/'+acc;d.type='text/javascript';d.async=true;d.defer=true; e.parentNode.insertBefore(d,e);})();
+});
+
+CoinWidgetCom.go({
+  wallet_address: "16bs6kN6wTVT7NUCMWxjCZ4UCTy3b9KVWX"
+  , currency: "bitcoin"
+  , counter: "count"
+  , alignment: "bl"
+  , qrcode: true
+  , auto_show: false
+  , lbl_button: "Donate BTC"
+  , lbl_address: "My Bitcoin Address:"
+  , lbl_count: "donations"
+  , lbl_amount: "BTC"
+});
+CoinWidgetCom.go({
+  wallet_address: "LYkd9Vo52NotPCpB6AUQ8Cg7sjF1meLsif"
+  , currency: "litecoin"
+  , counter: "count"
+  , alignment: "bl"
+  , qrcode: true
+  , auto_show: false
+  , lbl_button: "Donate LTC"
+  , lbl_address: "My Litecoin Address:"
+  , lbl_count: "donations"
+  , lbl_amount: "LTC"
 });
